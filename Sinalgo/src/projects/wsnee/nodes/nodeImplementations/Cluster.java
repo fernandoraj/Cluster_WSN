c5ae576ada6e3962a3f05c32e78db57d72813a20
fernandorodrigues.ufc@gmail.com <fernandorodrigues.ufc@gmail.com@9623baf7-9890-4137-8563-edf4c73deeb0>
2012-10-31 20:37:40 +0000

    Class created to group sensor nodes with similar readings

---
+++ Sinalgo/src/projects/wsnee/nodes/nodeImplementations/Cluster.java
@@ -0,0 +1,24 @@
+package projects.wsnee.nodes.nodeImplementations;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+
+public class Cluster //extends ArrayList<SimpleNode>
+{
+	/**
+	 * Maximum distance acceptable to the formation of clusters. If it is equal to zero (0.0), ignoring
+	 */
+	private double maxDistance = 0.0;
+	
+	public Cluster(long serial)
+	{
+//		this.setColor(Color.RED);
+//		serialVersionUID = serial;
+	}
+	
+	public ArrayList<SimpleNode> members;
+	
+//	static final long serialVersionUID;
+
+}
