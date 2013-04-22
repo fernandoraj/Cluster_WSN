a8564b7dbdf4c417d7171cd37b4ffbf39a36b9b1
fernandorodrigues.ufc@gmail.com <fernandorodrigues.ufc@gmail.com@9623baf7-9890-4137-8563-edf4c73deeb0>
2013-04-22 14:47:47 +0000

    Update in "inDebugMode" attribute value.

--- Sinalgo/src/projects/wsnee/utils/Utils.java
+++ Sinalgo/src/projects/wsnee/utils/Utils.java
@@ -10,7 +10,7 @@ public class Utils {
 	/**
 	 * the mode of debug is used to print debug messages that are spread throughout the project.
 	 */
-	private static boolean inDebugMode = true;
+	private static boolean inDebugMode = false;
 	
 	/**
 	 * Print debug messages only if the <code>inDebugMode</code> static variable is <code>true</code>.
