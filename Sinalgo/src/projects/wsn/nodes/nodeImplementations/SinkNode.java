c3aeea668cd8aed794c6b8bd64b78b058c3a8e74
fciago94 <fciago94@9623baf7-9890-4137-8563-edf4c73deeb0>
2013-10-23 14:56:46 +0000

    Tradução dos comentários

--- Sinalgo/src/projects/wsn/nodes/nodeImplementations/SinkNode.java
+++ Sinalgo/src/projects/wsn/nodes/nodeImplementations/SinkNode.java
@@ -16,22 +16,26 @@ public class SinkNode extends SimpleNode
 {
 
 	/**
-	 * Numero de dados sensoreados por time slot (Tamanho do time slot) 
+	 * Sensed data number per slot (size of the time slot)<p>
+	 * [Eng] Numero de dados sensoreados por time slot (Tamanho do time slot) 
 	 */
 	private Integer sizeTimeSlot = 100;
 	
 	/**
-	 * Tipo de dado a ser sensoreado (lido nos nós sensores), que pode ser: "t"=temperatura, "h"=humidade, "l"=luminosidade ou "v"=voltagem
+	 * Tipo de dado a ser sensoreado (lido nos nós sensores), que pode ser: "t"=temperatura, "h"=humidade, "l"=luminosidade ou "v"=voltagem<p>
+	 * Data type to be sensed (read in the sensor nodes), it can be: "t"=temperature, "h"=humidity, "l"=luminosity or "v"=voltage
 	 */
 	private String dataSensedType = "t"; // type (abbrev.)
 	
 	/**
-	 * Percentual do limiar de erro aceitável para as leituras dos nós sensores, que pode estar entre 0.0 (não aceita erros) e 1.0 (aceita todo e qualquer erro)
+	 * Percentual do limiar de erro aceitável para as leituras dos nós sensores, que pode estar entre 0.0 (não aceita erros) e 1.0 (aceita todo e qualquer erro)<p>
+	 * [Eng] Percentage of the possible threshold error to the sensor nodes readings, it can be between 0.0 (don't accept errors) and 1.0 (accept all and any errors)
 	 */
 	private double thresholdError = 0.05; // te (abbrev.)
 	
 	/**
-	 * Indicates what type of approach the sink node should signal to all other nodes must follow (Adaga-P*, continuously sensing-and-send (naive with or without learning initial time))
+	 * Indica que tipo de aproximação o sink node deve sinalizar que todos os outros nós devem seguir (Adaga-P*, continuamente sensorando-e-enviando (naive sabendo ou não os tempos iniciais) <p>
+	 * [Eng] Indicates what type of approach the sink node should signal to all other nodes must follow (Adaga-P*, continuously sensing-and-send (naive with or without learning initial time))
 	 */
 	private Integer approachType = 0; // 0(default) = temporal correlation (Adaga-P*); 2 = Naive
 	
@@ -96,9 +100,9 @@ public class SinkNode extends SimpleNode
 	 * [Eng] Receives the message, reads the parameters (items) in
 	 * dataRecordItens, calculates the coefficients A and B according to these
 	 * parameters and sends these coefficients for the sensor node of origin
-	 * @param wsnMsgResp Mensagem recebida com os parâmetros a serem lidos
-	 * @param sizeTimeSlot Tamanho do vetor de dados (série temporal) da mensagem
-	 * @param dataSensedType Tipo de dados (da série temporal) da mensagem
+	 * @param wsnMsgResp Mensagem recebida com os parâmetros a serem lidos <p> [Eng] <b>wsnMsgResp</b> Received message with the param to be read
+	 * @param sizeTimeSlot Tamanho do vetor de dados (série temporal) da mensagem <p> [Eng] <b>sizeTimeSlot</b> Size of the data array (time serie) of the message
+	 * @param dataSensedType Tipo de dados (da série temporal) da mensagem <p> [Eng] <b>dataSensedType</b> Data type (of the time serie) of the message
 	 */
 	private void receiveMessage(WsnMsgResponse wsnMsgResp, Integer sizeTimeSlot, String dataSensedType)
 	{
@@ -128,9 +132,10 @@ public class SinkNode extends SimpleNode
 	}
 	
 	/**
-	 * Calcula e retorna a média aritmética dos valores reais passados
-	 * @param values Array de valores reais de entrada 
-	 * @return Média dos valores reais de entrada
+	 * Calcula e retorna a média aritmética dos valores reais passados <p>
+	 * [Eng] Calculate and return the arithmetic average of the passed double values
+	 * @param values Array de valores reais de entrada <p> [Eng] <b>values</b> Array of the input double values 
+	 * @return Média dos valores reais de entrada <p> [Eng] Average of the input double values
 	 */
 	private double calculaMedia(double[] values)
 	{
@@ -147,12 +152,13 @@ public class SinkNode extends SimpleNode
 	}
 			
 	/**
-	 * Calcula o coeficiente B da equação de regressão
-	 * @param valores Array de valores (grandezas) das medições dos sensores
-	 * @param tempos Array de tempos das medições dos sensores
-	 * @param mediaValores Média dos valores
-	 * @param mediaTempos Média dos tempos
-	 * @return Valor do coeficiente B da equação de regressão
+	 * Calcula o coeficiente B da equação de regressão <p>
+	 * [Eng] Calculate the coefficient B of the regression equation
+	 * @param valores Array de valores (grandezas) das medições dos sensores <p> [Eng] <b>valores</b> Array of values of the sensor measures
+	 * @param tempos Array de tempos das medições dos sensores <p> [Eng] <b>tempos</b> Array of times of the sensor measures
+	 * @param mediaValores Média dos valores <p> [Eng] <b>mediaValores</b> Average of the values
+	 * @param mediaTempos Média dos tempos <p> [Eng] <b>mediaTempos</b> Average of the times
+	 * @return Valor do coeficiente B da equação de regressão <p> [Eng] Value of the B coefficient of the regression equation
 	 */
 	private double calculaB(double[] valores, double[] tempos, double mediaValores, double mediaTempos)
 	{
@@ -171,11 +177,12 @@ public class SinkNode extends SimpleNode
 	}
 	
 	/**
-	 * Calcula o coeficiente A da equação de regressão
-	 * @param mediaValores Média dos valores lidos pelos sensores
-	 * @param mediaTempos Média dos tempos de leitura dos valores pelos sensores
-	 * @param B Valor do coeficiente B da equação de regressão
-	 * @return Valor do coeficiente A
+	 * Calcula o coeficiente A da equação de regressão <p>
+	 * [Eng] Calculate the A coefficient of the regression equation
+	 * @param mediaValores Média dos valores lidos pelos sensores <p> [Eng] <b>mediaValores</b> Average of the values read by the sensors
+	 * @param mediaTempos Média dos tempos de leitura dos valores pelos sensores <p> [Eng] <b>mediaTempos</b> Average of the times of value readings by the sensors
+	 * @param B Valor do coeficiente B da equação de regressão <p> [Eng] <b>B</b> Value of the coefficient B of the regression equation
+	 * @return Valor do coeficiente A <p> [Eng] Value of the A coefficient
 	 */
 	private double calculaA(double mediaValores, double mediaTempos, double B)
 	{
