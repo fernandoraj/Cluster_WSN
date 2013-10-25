89f8fa484e82e4926bfd82acefb2b8cd94daa74e
marioronildo52 <marioronildo52@9623baf7-9890-4137-8563-edf4c73deeb0>
2013-10-25 19:13:05 +0000

    (SinkNode) organization and translation of variables and methods to english; comments on the end of each methods and tests.

--- Sinalgo/src/projects/wsn/nodes/nodeImplementations/SinkNode.java
+++ Sinalgo/src/projects/wsn/nodes/nodeImplementations/SinkNode.java
@@ -12,8 +12,7 @@ import sinalgo.nodes.messages.Inbox;
 import sinalgo.nodes.messages.Message;
 import sinalgo.runtime.Global;
 
-public class SinkNode extends SimpleNode 
-{
+public class SinkNode extends SimpleNode  {
 
 	/**
 	 * Numero de dados sensoriados por time slot (Tamanho do time slot) <p> 
@@ -50,45 +49,39 @@ public class SinkNode extends SimpleNode
 		Utils.printForDebug("The size of delay to send novelties is "+SimpleNode.limitPredictionError);
 		Utils.printForDebug("The approach type is "+approachType+" (0 = temporal correlation (Adaga-P*); 2 = Naive)");
 		
-//		if(LogL.ROUND_DETAIL){
 			Global.log.logln("\nThe size of time slot is "+sizeTimeSlot);
 			Global.log.logln("The type of data sensed is "+dataSensedType);
 			Global.log.logln("The threshold of error (max error) is "+thresholdError);
 			Global.log.logln("The size of sliding window is "+SimpleNode.slidingWindowSize);
 			Global.log.logln("The size of delay to send novelties is "+SimpleNode.limitPredictionError+"\n");
 			Global.log.logln("The approach type is "+approachType+" (0 = temporal correlation (Adaga-P*); 2 = Naive)");
-//		}
 	}
 
 	@Override
 	public void draw(Graphics g, PositionTransformation pt, boolean highlight) {
 		String text = "S";
 		super.drawNodeAsSquareWithText(g, pt, highlight, text, 1, Color.WHITE);
-	}
+	} // end draw(Graphics g, PositionTransformation pt, boolean highlight)
 	
 	@NodePopupMethod(menuText="Definir Sink como Raiz de Roteamento")
 	public void construirRoteamento(){
 		this.proximoNoAteEstacaoBase = this;
-		//WsnMsg wsnMessage = new WsnMsg(1, this, null, this, 0); //Integer seqID, Node origem, Node destino, Node forwardingHop, Integer tipo
 		WsnMsg wsnMessage = new WsnMsg(1, this, null, this, approachType, sizeTimeSlot, dataSensedType); 
 		WsnMessageTimer timer = new WsnMessageTimer(wsnMessage);
 		timer.startRelative(1, this);
-	}
+	} // end construirRoteamento()
 	
 	@Override
-	public void handleMessages(Inbox inbox) 
-	{
-		while (inbox.hasNext())
-		{
+	public void handleMessages(Inbox inbox) {
+		while (inbox.hasNext()) {
 			Message message = inbox.next();
-			if (message instanceof WsnMsgResponse)
-			{
+			if (message instanceof WsnMsgResponse) {
 				this.setColor(Color.YELLOW);
 				WsnMsgResponse wsnMsgResp = (WsnMsgResponse) message;
 				receiveMessage(wsnMsgResp, wsnMsgResp.sizeTimeSlot, wsnMsgResp.dataSensedType);
-			} //if (message instanceof WsnMsg)
-		} //while (inbox.hasNext())
-	} //public void handleMessages
+			} // end if (message instanceof WsnMsg)
+		} // end while (inbox.hasNext())
+	} // end  handleMessages (Inbox inbox)
 	
 	/**
 	 * Recebe a mensagem passada. Caso a abordagem utilizada seja a de correlação
@@ -104,32 +97,29 @@ public class SinkNode extends SimpleNode
 	 * @param sizeTimeSlot Tamanho do vetor de dados (série temporal) da mensagem <p> [Eng] <b>sizeTimeSlot</b> Size of the data array (time serie) of the message
 	 * @param dataSensedType Tipo de dados (da série temporal) da mensagem <p> [Eng] <b>dataSensedType</b> Data type (of the time serie) of the message
 	 */
-	private void receiveMessage(WsnMsgResponse wsnMsgResp, Integer sizeTimeSlot, String dataSensedType)
-	{
-		if (approachType == 2){ // Abordagem Naive
+	private void receiveMessage(WsnMsgResponse wsnMsgResp, Integer sizeTimeSlot, String dataSensedType) {
+		if (approachType == 2) { // Abordagem Naive
 			// Tratar mensagem recebida com dados do sensor
-		}
-		else if (wsnMsgResp != null && wsnMsgResp.dataRecordItens != null){
+		} // end if (approachType == 2)
+		else if (wsnMsgResp != null && wsnMsgResp.dataRecordItens != null) {
 			int size = wsnMsgResp.dataRecordItens.size();
-			double[] valores = new double[size];
-			double[] tempos = new double[size];
-//			char[] tipos = new char[size];
+			double[] values = new double[size];
+			double[] times = new double[size];
 			//Dados lidos do sensor correspondente
-			valores = wsnMsgResp.getDataRecordValues();
-			tempos = wsnMsgResp.getDataRecordTimes();
-//			tipos = wsnMsgResp.getDataRecordTypes();
+			values = wsnMsgResp.getDataRecordValues();
+			times = wsnMsgResp.getDataRecordTimes();
 			//Coeficientes de regressão linear com os vetores acima
-			double coeficienteA, coeficienteB;
-			double mediaTempos, mediaValores;
+			double coefficientA, coefficientB;
+			double averageTimes, averageValues;
 			//Médias dos valores de leitura e tempos
-			mediaTempos = calculaMedia(tempos);
-			mediaValores = calculaMedia(valores);
+			averageTimes = calculatesAverage(times);
+			averageValues = calculatesAverage(values);
 			//Cálculos dos coeficientes de regressão linear com os vetores acima
-			coeficienteB = calculaB(valores, tempos, mediaValores, mediaTempos);
-			coeficienteA = calculaA(mediaValores, mediaTempos, coeficienteB);
-			sendCoefficients(wsnMsgResp, coeficienteA, coeficienteB);
-		}
-	}
+			coefficientB = calculateB(values, times, averageValues, averageTimes);
+			coefficientA = calculateA(averageValues, averageTimes, coefficientB);
+			sendCoefficients(wsnMsgResp, coefficientA, coefficientB);
+		} // end if (wsnMsgResp != null && wsnMsgResp.dataRecordItens != null)
+	} // end void receiveMessage(WsnMsgResponse wsnMsgResp, Integer sizeTimeSlot, String dataSensedType)
 	
 	/**
 	 * Calcula e retorna a média aritmética dos valores reais passados <p>
@@ -137,63 +127,55 @@ public class SinkNode extends SimpleNode
 	 * @param values Array de valores reais de entrada <p> [Eng] <b>values</b> Array of the input double values 
 	 * @return Média dos valores reais de entrada <p> [Eng] Average of the input double values
 	 */
-	private double calculaMedia(double[] values)
-	{
+	private double calculatesAverage(double[] values) {
 		double mean = 0, sum = 0;
-		for (int i=0; i<values.length; i++)
-		{
+		for (int i=0; i<values.length; i++) {
 			sum += values[i];
-		}
-		if (values.length > 0)
-		{
+		} // end for (int i=0; i<values.length; i++)
+		if (values.length > 0) {
 			mean = sum/values.length;
-		}
+		} // end if (values.length > 0)
 		return mean;
-	}
+	} // end calculatesAverage(double[] values)
 			
 	/**
 	 * Calcula o coeficiente B da equação de regressão <p>
 	 * [Eng] Calculate the coefficient B of the regression equation
-	 * @param valores Array de valores (grandezas) das medições dos sensores <p> [Eng] <b>valores</b> Array of values of the sensor measures
-	 * @param tempos Array de tempos das medições dos sensores <p> [Eng] <b>tempos</b> Array of times of the sensor measures
-	 * @param mediaValores Média dos valores <p> [Eng] <b>mediaValores</b> Average of the values
-	 * @param mediaTempos Média dos tempos <p> [Eng] <b>mediaTempos</b> Average of the times
+	 * @param values Array de valores (grandezas) das medições dos sensores <p> [Eng] <b>valores</b> Array of values of the sensor measures
+	 * @param times Array de tempos das medições dos sensores <p> [Eng] <b>tempos</b> Array of times of the sensor measures
+	 * @param averageValues Média dos valores <p> [Eng] <b>mediaValores</b> Average of the values
+	 * @param averageTimes Média dos tempos <p> [Eng] <b>mediaTempos</b> Average of the times
 	 * @return Valor do coeficiente B da equação de regressão <p> [Eng] Value of the B coefficient of the regression equation
 	 */
-	private double calculaB(double[] valores, double[] tempos, double mediaValores, double mediaTempos)
-	{
-		double numerador = 0.0, denominador = 0.0, x;
-		for (int i = 0; i < tempos.length; i++)
-		{
-			x = tempos[i] - mediaTempos;
-			numerador += x*(valores[i] - mediaValores);
-			denominador += x*x;
-		}
-		if (denominador != 0)
-		{
-			return (numerador/denominador);
-		}
+	private double calculateB(double[] values, double[] times, double averageValues, double averageTimes) {
+		double numerator = 0.0, denominator = 0.0, x;
+		for (int i = 0; i < times.length; i++) {
+			x = times[i] - averageTimes;
+			numerator += x*(values[i] - averageValues);
+			denominator += x*x;
+		} // end for (int i = 0; i < times.length; i++)
+		if (denominator != 0) {
+			return (numerator/denominator);
+		} // end if (denominator != 0)
 		return 0.0;
-	}
+	} // end calculateB(double[] values, double[] times, double averageValues, double averageTimes)
 	
 	/**
 	 * Calcula o coeficiente A da equação de regressão <p>
 	 * [Eng] Calculate the A coefficient of the regression equation
-	 * @param mediaValores Média dos valores lidos pelos sensores <p> [Eng] <b>mediaValores</b> Average of the values read by the sensors
-	 * @param mediaTempos Média dos tempos de leitura dos valores pelos sensores <p> [Eng] <b>mediaTempos</b> Average of the times of value readings by the sensors
+	 * @param averageValues Média dos valores lidos pelos sensores <p> [Eng] <b>mediaValores</b> Average of the values read by the sensors
+	 * @param averageTimes Média dos tempos de leitura dos valores pelos sensores <p> [Eng] <b>mediaTempos</b> Average of the times of value readings by the sensors
 	 * @param B Valor do coeficiente B da equação de regressão <p> [Eng] <b>B</b> Value of the coefficient B of the regression equation
 	 * @return Valor do coeficiente A <p> [Eng] Value of the A coefficient
 	 */
-	private double calculaA(double mediaValores, double mediaTempos, double B)
-	{
-		return (mediaValores - B*mediaTempos);
-	}
+	private double calculateA(double averageValues, double averageTimes, double B) {
+		return (averageValues - B*averageTimes);
+	} // end calculateA(double averageValues, double averageTimes, double B)
 	
-	private void sendCoefficients(WsnMsgResponse wsnMsgResp, double coeficienteA, double coeficienteB)
-	{
+	private void sendCoefficients(WsnMsgResponse wsnMsgResp, double coefficientA, double coefficientB) {
 		WsnMsg wsnMessage = new WsnMsg(1, this, wsnMsgResp.source , this, 1, 1, dataSensedType, thresholdError);
-		wsnMessage.setCoefs(coeficienteA, coeficienteB);
+		wsnMessage.setCoefs(coefficientA, coefficientB);
 		wsnMessage.setPathToSenderNode(wsnMsgResp.clonePath());
 		sendToNextNodeInPath(wsnMessage);
-	}
-}
+	} // end sendCoefficients(WsnMsgResponse wsnMsgResp, double coefficientA, double coefficientB)
+} // end SinkNode class
