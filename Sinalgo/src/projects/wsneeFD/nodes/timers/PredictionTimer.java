e81407fac1566f3b7e56c6f2a04740a6d4a7d39f
fernandorodrigues.ufc@gmail.com <fernandorodrigues.ufc@gmail.com@9623baf7-9890-4137-8563-edf4c73deeb0>
2014-08-01 22:34:30 +0000

    New class PredictionTimer without "internal values", i.e., with "hot swap" of the coeffs. and other parameters that must be stored (saved) by the current sensor.

--- Sinalgo/src/projects/wsneeFD/nodes/timers/PredictionTimer.java
+++ Sinalgo/src/projects/wsneeFD/nodes/timers/PredictionTimer.java
@@ -5,22 +5,22 @@ import sinalgo.nodes.timers.Timer;
 
 public class PredictionTimer extends Timer {
 
-	private int[] dataTypes;
+/*	private int[] dataTypes;
 	private double[] As;
 	private double[] Bs;
 	private double[] errors;
-	
-	public PredictionTimer(int[] dataSensedTypes, double[] coefsA, double[] coefsB, double[] maxErrors)
+*/	
+	public PredictionTimer() //int[] dataSensedTypes, double[] coefsA, double[] coefsB, double[] maxErrors)
 	{
-		this.dataTypes = dataSensedTypes;
+/*		this.dataTypes = dataSensedTypes;
 		this.As = coefsA;
 		this.Bs = coefsB;
 		this.errors = maxErrors;
-	}
+*/	}
 	
 	@Override
 	public void fire() {
-		((SimpleNode)node).triggerPrediction(dataTypes, As, Bs, errors);
+		((SimpleNode)node).triggerPrediction(); //(dataTypes, As, Bs, errors);
 	}
 
 }
