71e000e717c68d26fa5d94c1e03756978f462459
rayonnunes <rayonnunes@warmachine.rayonnunes.local>
2016-09-17 05:44:32 -0300

    MessageItem messageItemsToSink to Vector<MessageItem> messageItemsToSink

--- Sinalgo/src/projects/wsneeFD/nodes/messages/WsnMsgResponse.java
+++ Sinalgo/src/projects/wsneeFD/nodes/messages/WsnMsgResponse.java
@@ -166,7 +166,7 @@ public class WsnMsgResponse extends Message {
 */	
 	public MessageItem messageItemToCH;
 	
-	public MessageItem messageItemsToSink;
+	public Vector<MessageItem> messageItemsToSink;
 
 /*
 	public void addDataRecordItens(int[] typs, double[] vals, double tim, double bat, int rnd)
